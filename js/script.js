8cc320f3f10bfd9d2616b3e32ba09c8dcc261956
Bazarova Anastasiya <nassi_13@inbox.ru>
2021-09-09 15:43:44 +0300

    new test js

--- js/script.js
+++ js/script.js
@@ -3,12 +3,15 @@ document.addEventListener('dblclick', function (e) {
 });
 
 
-window.onload = function() {
+
   var video = document.querySelectorAll('video');
   video.addEventListener( 'pause' , function () {
         if (video.paused === true)
         {
             video.play();}
   });
-  
-};
+  window.addEventListener('message', function (event) {
+     if (event.data.method === 'play') {
+         document.querySelectorAll('video').play();
+     }
+});
