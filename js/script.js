834c0b5e070bceb28beb12acb5920ac78504ab71
Bazarova Anastasiya <nassi_13@inbox.ru>
2021-09-09 13:49:53 +0300

    fix js&html

--- js/script.js
+++ js/script.js
@@ -3,28 +3,8 @@ document.addEventListener('dblclick', function (e) {
 });
 
 
-$(document).ready(function () {
-  
-    $('.main__slider').slick({
-        draggable:false,
-        swipe: false,
-        arrows: false,
-        dots: false,
-        infinite: true,
-        fade: true,
-
-        autoplay: true,
-        autoplaySpeed: 4000,
-          centerMode: true,
-        slidesToShow: 1, 
-    });
-    
-});
-document.getElementById("next-1").onclick = function () {
-  (document.getElementById("page__r2").style.display = "flex"),
-   (document.getElementById("page__r1").style.display = "none");
-};
-document.getElementById("next-2").onclick = function () {
-  (document.getElementById("page__r3").style.display = "flex"),
-   (document.getElementById("page__r2").style.display = "none");
+window.onload = function() {
+  var elem = document.querySelectorAll('video');
+  var srcVideo = elem.getAttribute('src');
+  elem.setAttribute('src', srcVideo + '&autoplay=1');
 };
