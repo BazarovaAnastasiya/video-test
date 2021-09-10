fca390a41fb30133a2fcb611205c17227c9e5d4c
Bazarova Anastasiya <nassi_13@inbox.ru>
2021-09-10 11:59:34 +0300

    fix btn

--- js/script.js
+++ js/script.js
@@ -3,38 +3,44 @@ document.addEventListener('dblclick', function (e) {
 });
 
 
-let video = document.getElementById('video');
-video.addEventListener('pause', () => {
-video.play();
-alert(play);
-})
+video.addEventListener('canplay', function(e) {
+this.volume = 0.4;
+this.currentTime = 10;
+this.play();
+}, false);
 
-video.addEventListener('focus', () => {
-video.play();
-alert(play);
-})
+// let video = document.getElementById('video');
+// video.addEventListener('pause', () => {
+// video.play();
+// alert(play);
+// })
 
-video.addEventListener('blur', () => {
-video.play();
-alert(noplay);
-})
-  var video = document.querySelectorAll('video');
+// video.addEventListener('focus', () => {
+// video.play();
+// alert(play);
+// })
 
-  video.addEventListener( 'pause' , function () {
-        if (video.paused === true)
-        {
-            video.play();}
-  });
-  window.addEventListener('onload', function () {
-     alert("я в фокусе");
-     if (video === 'pause') {
-         document.querySelectorAll('video').play();
-     }
-});
-  window.addEventListener('focus', function (event) {
-  alert("привет");
-         document.querySelectorAll('video').play();
-         event.preventDefault();
-    event.stopPropagation();
+// video.addEventListener('blur', () => {
+// video.play();
+// alert(noplay);
+// })
+//   var video = document.querySelectorAll('video');
+
+//   video.addEventListener( 'pause' , function () {
+//         if (video.paused === true)
+//         {
+//             video.play();}
+//   });
+//   window.addEventListener('onload', function () {
+//      alert("я в фокусе");
+//      if (video === 'pause') {
+//          document.querySelectorAll('video').play();
+//      }
+// });
+//   window.addEventListener('focus', function (event) {
+//   alert("привет");
+//          document.querySelectorAll('video').play();
+//          event.preventDefault();
+//     event.stopPropagation();
   
-});
+// });
