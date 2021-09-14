f8bd161654718e76eef18c764cd3da69c146bfdf
Bazarova Anastasiya <nassi_13@inbox.ru>
2021-09-14 15:56:01 +0300

    fix btn

--- js/script.js
+++ js/script.js
@@ -23,16 +23,13 @@ document.addEventListener('dblclick', function (e) {
 // // this.play();
 // }, false);
    
-// video.addEventListener('pause', () => {
-// video.play();
-// })
+video.addEventListener('pause', () => {
+   alert("canplay");
+video.play();
+})
 
  window.addEventListener('focus', () => {
        video.play();
-       if(video.paused){
-         alert("pays");
-        video().play();
-    }
 });
 
 // //    var video = document.querySelector('video');
